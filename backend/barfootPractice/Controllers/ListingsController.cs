b9a0c93fba022da132de74d110a4bd40e3826419
sOm2y <yyin888@aucklanduni.ac.nz>
2019-08-11 12:44:58 +1200

    add staffs and listings controller

---
+++ backend/barfootPractice/Controllers/ListingsController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using barfootPractice.Models;
+
+namespace barfootPractice.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Listings")]
+    public class ListingsController : Controller
+    {
+        private readonly BarfootContext _context;
+
+        public ListingsController(BarfootContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Listings
+        [HttpGet]
+        public IEnumerable<Listing> GetListings()
+        {
+            return _context.Listings;
+        }
+
+        // GET: api/Listings/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetListing([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var listing = await _context.Listings.SingleOrDefaultAsync(m => m.ListingId == id);
+
+            if (listing == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(listing);
+        }
+
+        // PUT: api/Listings/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutListing([FromRoute] int id, [FromBody] Listing listing)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != listing.ListingId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(listing).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ListingExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Listings
+        [HttpPost]
+        public async Task<IActionResult> PostListing([FromBody] Listing listing)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Listings.Add(listing);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetListing", new { id = listing.ListingId }, listing);
+        }
+
+        // DELETE: api/Listings/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteListing([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var listing = await _context.Listings.SingleOrDefaultAsync(m => m.ListingId == id);
+            if (listing == null)
+            {
+                return NotFound();
+            }
+
+            _context.Listings.Remove(listing);
+            await _context.SaveChangesAsync();
+
+            return Ok(listing);
+        }
+
+        private bool ListingExists(int id)
+        {
+            return _context.Listings.Any(e => e.ListingId == id);
+        }
+    }
+}
---
+++ backend/barfootPractice/Controllers/StaffsController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using barfootPractice.Models;
+
+namespace barfootPractice.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Staffs")]
+    public class StaffsController : Controller
+    {
+        private readonly BarfootContext _context;
+
+        public StaffsController(BarfootContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Staffs
+        [HttpGet]
+        public IEnumerable<Staff> GetStaffs()
+        {
+            return _context.Staffs;
+        }
+
+        // GET: api/Staffs/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetStaff([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var staff = await _context.Staffs.SingleOrDefaultAsync(m => m.StaffId == id);
+
+            if (staff == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(staff);
+        }
+
+        // PUT: api/Staffs/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutStaff([FromRoute] int id, [FromBody] Staff staff)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != staff.StaffId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(staff).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StaffExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Staffs
+        [HttpPost]
+        public async Task<IActionResult> PostStaff([FromBody] Staff staff)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Staffs.Add(staff);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetStaff", new { id = staff.StaffId }, staff);
+        }
+
+        // DELETE: api/Staffs/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteStaff([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var staff = await _context.Staffs.SingleOrDefaultAsync(m => m.StaffId == id);
+            if (staff == null)
+            {
+                return NotFound();
+            }
+
+            _context.Staffs.Remove(staff);
+            await _context.SaveChangesAsync();
+
+            return Ok(staff);
+        }
+
+        private bool StaffExists(int id)
+        {
+            return _context.Staffs.Any(e => e.StaffId == id);
+        }
+    }
+}
