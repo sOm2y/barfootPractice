267bc057afa64b25ac5376dceaa1e5ddc982253f
sOm2y <yyin888@aucklanduni.ac.nz>
2019-08-11 14:24:10 +1200

    add userService for user role management

---
+++ backend/barfootPractice/Services/UserService.cs
@@ -0,0 +1,70 @@
+﻿using barfootPractice.Models;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace barfootPractice.Services
+{
+    public interface IUserService
+    {
+        User Authenticate(string username, string password);
+        User GetById(int id);
+    }
+
+    public class UserService : IUserService
+    {
+        private readonly BarfootContext _context;
+
+        public UserService(BarfootContext context)
+        {
+            _context = context;
+        }
+
+        public User Authenticate(string username, string password)
+        {
+            var user = _context.Users.SingleOrDefault(x => x.Username == username && x.Password == password);
+
+            // return null if user not found
+            if (user == null)
+                return null;
+
+            // authentication successful so generate jwt token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes("secret");
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Id.ToString()),
+                    new Claim(ClaimTypes.Role, user.Role)
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            user.Token = tokenHandler.WriteToken(token);
+
+            // remove password before returning
+            user.Password = null;
+
+            return user;
+        }
+
+        public User GetById(int id)
+        {
+            var user = _context.Users.FirstOrDefault(x => x.Id == id);
+
+            // return user without password
+            if (user != null)
+                user.Password = null;
+
+            return user;
+        }
+    }
+}
