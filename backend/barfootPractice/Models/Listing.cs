0feeebf39e973cc755ca96b72cb36a6982d6fb0e
sOm2y <yyin888@aucklanduni.ac.nz>
2019-08-11 12:23:31 +1200

    add entities

---
+++ backend/barfootPractice/Models/Listing.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace barfootPractice.Models
+{
+    public class Listing
+    {
+        public string Address { get; set; }
+        public float Price { get; set; }
+    }
+}
---
+++ backend/barfootPractice/Models/StaffRole.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace barfootPractice.Models
+{
+    public static class StaffRole
+    {
+        public const string Sales = "Sales";
+        public const string SalesAdmin = "SalesAdmin";
+        public const string SalesDepartmentAdmin = "SalesDepartmentAdmin";
+    }
+}
