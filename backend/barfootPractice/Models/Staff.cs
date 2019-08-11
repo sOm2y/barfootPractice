8577a5db6581466913868d34c461e77a29f29b6c
sOm2y <yyin888@aucklanduni.ac.nz>
2019-08-11 13:58:44 +1200

    remove role in staff entity

--- backend/barfootPractice/Models/Staff.cs
+++ backend/barfootPractice/Models/Staff.cs
@@ -11,6 +11,5 @@ namespace barfootPractice.Models
         public string Name { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
-        public string Role { get; set; }
     }
 }
