843836c784cedcd38615c05d2047e9a2306e4ea1
sOm2y <yyin888@aucklanduni.ac.nz>
2019-08-11 17:51:21 +1200

    add seed data

--- backend/barfootPractice/Migrations/20190811053541_UpdateTestDataAndAddAnnotation.Designer.cs
+++ backend/barfootPractice/Migrations/20190811053541_UpdateTestDataAndAddAnnotation.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using barfootPractice.Models;
 
 namespace barfootPractice.Migrations
 {
     [DbContext(typeof(BarfootContext))]
-    partial class BarfootContextModelSnapshot : ModelSnapshot
+    [Migration("20190811053541_UpdateTestDataAndAddAnnotation")]
+    partial class UpdateTestDataAndAddAnnotation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- backend/barfootPractice/Models/BarfootContext.cs
+++ backend/barfootPractice/Models/BarfootContext.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
 
 namespace barfootPractice.Models
 {
@@ -12,5 +14,36 @@ namespace barfootPractice.Models
         public DbSet<Listing> Listings { get; set; }
         public DbSet<Staff> Staffs { get; set; }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+             .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+             .AddJsonFile("appsettings.json")
+             .Build();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("barfoot"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Staff>().HasData(
+                new Staff() { StaffId = 1, Email = "test1@barfoot.co.nz", Name = "test1", Phone = "123456" },
+                new Staff() { StaffId = 2, Email = "test1@barfoot.co.nz", Name = "test1", Phone = "123456" },
+                new Staff() { StaffId = 3, Email = "test1@barfoot.co.nz", Name = "test1", Phone = "123456" });
+
+            modelBuilder.Entity<Listing>().HasData(
+                new Listing() { ListingId = 1,  Address = "10 auckland street, auckland, 1010", Price = 1200000, Status = "open", ConfidentialNote = "buyer expectation is under 1m.", StaffId = 1 },
+                new Listing() { ListingId = 2,  Address = "10 auckland street, auckland, 1010", Price = 1200000, Status = "open", ConfidentialNote = "buyer expectation is under 1m.", StaffId = 2 },
+                new Listing() { ListingId = 3,  Address = "10 auckland street, auckland, 1010", Price = 1200000, Status = "open", ConfidentialNote = "buyer expectation is under 1m.", StaffId = 3});
+
+     
+
+            modelBuilder.Entity<User>().HasData(
+                new User() { Id = 1, FirstName = "Sales",                StaffId=1,   LastName = "User", Username = "sales",                  Password = "sales", Role = StaffRole.Sales }, 
+                new User() { Id = 2, FirstName = "SalesAdmin",           StaffId=2,   LastName = "User", Username = "salesAdmin",             Password = "salesAdmin",  Role = StaffRole.SalesAdmin },
+                new User() { Id = 3, FirstName = "SalesDepartmentAdmin", StaffId=3,   LastName = "User", Username = "salesDepartmentAdmin",   Password = "salesDepartmentAdmin",  Role = StaffRole.SalesDepartmentAdmin });
+
+   
+
+        }
     }
 }
