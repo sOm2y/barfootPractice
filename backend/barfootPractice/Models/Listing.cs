a15bd9dfc08466bf8d683184e45fcad305d99392
sOm2y <yyin888@aucklanduni.ac.nz>
2019-08-11 12:39:39 +1200

    add ids to entities

--- backend/barfootPractice/Models/Listing.cs
+++ backend/barfootPractice/Models/Listing.cs
@@ -7,6 +7,7 @@ namespace barfootPractice.Models
 {
     public class Listing
     {
+        public int ListingId { get; set; }
         public string Address { get; set; }
         public float Price { get; set; }
     }
--- backend/barfootPractice/Models/Staff.cs
+++ backend/barfootPractice/Models/Staff.cs
@@ -7,8 +7,10 @@ namespace barfootPractice.Models
 {
     public class Staff
     {
+        public int StaffId { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
+        public string Role { get; set; }
     }
 }
