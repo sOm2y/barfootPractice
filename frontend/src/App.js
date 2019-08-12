34528a85acb9dfc10762a704e47753c6096e25f7
sOm2y <yyin888@aucklanduni.ac.nz>
2019-08-12 20:45:46 +1200

    add main page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Layout, Menu, Breadcrumb } from 'antd'
+import './App.css'
+import WrappedNormalLoginForm from './components/forms/login';
+const { Header, Content, Footer } = Layout
 
-function App() {
+const App = ({ ...props })  => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Layout className="layout">
+      <Header>
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['2']}
+          style={{ lineHeight: '64px' }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Menu.Item key="1">nav 1</Menu.Item>
+          <Menu.Item key="2">nav 2</Menu.Item>
+          <Menu.Item key="3">nav 3</Menu.Item>
+        </Menu>
+      </Header>
+      <Content style={{ padding: '0 50px' }}>
+        <Breadcrumb style={{ margin: '16px 0' }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <div style={{ background: '#fff', padding: 24, minHeight: 280, maxWidth:1000, margin:"0 auto" }}>
+          <WrappedNormalLoginForm />
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Barfoot Practice ©2019 Created by Yue</Footer>
+    </Layout>
   );
 }
 
-export default App;
+export default App
