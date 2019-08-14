51ecfb70371d2757d93594968b288ddfd4152357
sOm2y <yyin888@aucklanduni.ac.nz>
2019-08-14 20:42:10 +1200

    automapper profile

---
+++ backend/barfootPractice/Mappings/MappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using barfootPractice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace barfootPractice.Mappings
+{
+    public class MappingProfile: Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Listing, ListingViewDto>();
+            CreateMap<ListingViewDto, Listing>();
+
+            CreateMap<Listing, ListingConfidentialViewDto>();
+            CreateMap<ListingConfidentialViewDto, Listing>();
+        }
+     
+    }
+}
